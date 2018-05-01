29447ec98c2e56f187db9366b0376c7f1e9e6cc0
RachelDelRey <rachelu92@icloud.com>
2018-05-01 04:49:38 -0500

    ru
    Updates to function passlist

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function passlist()
    var getshorturl=0;
    var login = "o_3iokgmm945";
    var api_key = "R_f2f3c9387a374e3fc6bf4b1ec2c945c4";
-   var long_url = "https://rvclist.github.io/index.html?list="+ shoppinglist;
+   var long_url = "https://ungerlist.github.io/index.html?list="+ shoppinglist;
   try{
   $.getJSON(
              "https://api-ssl.bitly.com/v3/shorten?callback=?",
